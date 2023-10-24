a28ab730c01936a7801ce7e048796ad49cd597ae
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2023-10-24 19:51:21 +0300

    import bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/js/bootstrap.js";
+import App from "./core/App.tsx";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
